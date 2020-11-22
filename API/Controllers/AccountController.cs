44281b73a2a10e8c072affcafa5e7df04da9d9a4
Tim Thompson <scubatim84@gmail.com>
2020-11-21 17:23:41 -0700

    Add dev token key to appsettings.Development.json, create UserDto class, add token service to AccountController and change return types to return UserDto

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography;
@@ -12,14 +13,16 @@ namespace API.Controllers
 	public class AccountController : BaseApiController
 	{
 		private readonly DataContext _context;
+		private readonly ITokenService _tokenService;
 
-		public AccountController(DataContext context)
+		public AccountController(DataContext context, ITokenService tokenService)
 		{
+			_tokenService = tokenService;
 			_context = context;
 		}
 
 		[HttpPost("register")]
-		public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+		public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
 		{
 			if (await UserExists(registerDto.UserName))
 			{
@@ -38,11 +41,15 @@ namespace API.Controllers
 			_context.Users.Add(user);
 			await _context.SaveChangesAsync();
 
-			return user;
+			return new UserDto
+			{
+				UserName = user.UserName,
+				Token = _tokenService.CreateToken(user)
+			};
 		}
 
 		[HttpPost("login")]
-		public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+		public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
 		{
 			var user = await _context.Users
 				.SingleOrDefaultAsync(user => user.UserName == loginDto.UserName);
@@ -61,7 +68,11 @@ namespace API.Controllers
 				}
 			}
 
-			return user;
+			return new UserDto
+			{
+				UserName = user.UserName,
+				Token = _tokenService.CreateToken(user)
+			};
 		}
 
 		private async Task<bool> UserExists(string username)
