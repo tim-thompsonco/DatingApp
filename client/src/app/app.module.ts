9bb91e9e8807ecbeb46f931aaf91a95f5f236013
Tim Thompson <scubatim84@gmail.com>
2020-11-23 07:51:56 -0700

    Add register form to home page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,9 +9,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, HomeComponent],
+  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
