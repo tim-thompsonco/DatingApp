ad637c60331945179d717aec8a47151ac8e9c18e
Tim Thompson <scubatim84@gmail.com>
2020-12-14 11:54:48 -0700

    Implement AutoMapper

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -1,7 +1,11 @@
+using API.DTOs;
 using API.Entities;
 using API.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace API.Data
@@ -9,11 +13,12 @@ namespace API.Data
 	public class UserRepository : IUserRepository
 	{
 		private readonly DataContext _context;
+		private readonly IMapper _mapper;
 
-		public UserRepository(DataContext context)
+		public UserRepository(DataContext context, IMapper mapper)
 		{
+			_mapper = mapper;
 			_context = context;
-
 		}
 
 		public async Task<AppUser> GetUserByIdAsync(int id)
@@ -23,12 +28,16 @@ namespace API.Data
 
 		public async Task<AppUser> GetUserByUsernameAsync(string username)
 		{
-			return await _context.Users.SingleOrDefaultAsync(user => user.UserName == username);
+			return await _context.Users
+				.Include(p => p.Photos)
+				.SingleOrDefaultAsync(user => user.UserName == username);
 		}
 
 		public async Task<IEnumerable<AppUser>> GetUsersAsync()
 		{
-			return await _context.Users.ToListAsync();
+			return await _context.Users
+				.Include(p => p.Photos)
+				.ToListAsync();
 		}
 
 		public async Task<bool> SaveAllAsync()
@@ -39,6 +48,21 @@ namespace API.Data
 		public void Update(AppUser user)
 		{
 			_context.Entry(user).State = EntityState.Modified;
+		}
+
+		public async Task<MemberDto> GetMemberAsync(string username)
+		{
+			return await _context.Users
+				.Where(user => user.UserName == username)
+				.ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
+				.SingleOrDefaultAsync();
+		}
+
+		public async Task<IEnumerable<MemberDto>> GetMembersAsync()
+		{
+			return await _context.Users
+				.ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
+				.ToListAsync();
 		}
 	}
 }
--- API/Interfaces/IUserRepository.cs
+++ API/Interfaces/IUserRepository.cs
@@ -1,3 +1,4 @@
+using API.DTOs;
 using API.Entities;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -11,5 +12,7 @@ namespace API.Interfaces
 		Task<IEnumerable<AppUser>> GetUsersAsync();
 		Task<AppUser> GetUserByIdAsync(int id);
 		Task<AppUser> GetUserByUsernameAsync(string username);
+		Task<IEnumerable<MemberDto>> GetMembersAsync();
+		Task<MemberDto> GetMemberAsync(string username);
 	}
 }
