c1affada7ef529e7f79c9c6c3c42b804cd804f51
Tim Thompson <scubatim84@gmail.com>
2020-12-24 11:27:27 -0700

    Implement retrieval of specific member data when profile button is clicked

--- client/src/app/services/members.service.ts
+++ client/src/app/services/members.service.ts
@@ -20,6 +20,6 @@ export class MembersService {
   }
 
   getMember(username: string): Observable<Member> {
-    return this.http.get<Member>(`${this.baseUrl}'users/'${username}`);
+    return this.http.get<Member>(`${this.baseUrl}users/${username}`);
   }
 }
