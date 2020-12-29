e469aae9f2367b871b3182a02f9082d6219c55d7
Tim Thompson <scubatim84@gmail.com>
2020-12-29 04:54:04 -0700

    Add update member functionality to member edit component and persist changes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -15,6 +15,7 @@ import { TestErrorsComponent } from './errors/test-errors/test-errors.component'
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { PreventUnsavedChangesGuard } from './guards/prevent-unsaved-changes.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -28,7 +29,11 @@ const routes: Routes = [
         component: MemberListComponent,
       },
       { path: 'members/:username', component: MemberDetailComponent },
-      { path: 'member/edit', component: MemberEditComponent },
+      {
+        path: 'member/edit',
+        component: MemberEditComponent,
+        canDeactivate: [PreventUnsavedChangesGuard],
+      },
       { path: 'lists', component: ListsComponent },
       { path: 'messages', component: MessagesComponent },
     ],
--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -1,5 +1,5 @@
 // libs
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
 import { take } from 'rxjs/operators';
 
 // models
@@ -9,6 +9,8 @@ import { User } from 'src/app/models/user';
 // services
 import { AccountService } from 'src/app/services/account.service';
 import { MembersService } from 'src/app/services/members.service';
+import { ToastrService } from 'ngx-toastr';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-member-edit',
@@ -16,12 +18,21 @@ import { MembersService } from 'src/app/services/members.service';
   styleUrls: ['./member-edit.component.css'],
 })
 export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm') editForm: NgForm;
   member: Member;
   user: User;
+  @HostListener('window:beforeunload', ['$event']) unloadNotification(
+    $event: any
+  ): void {
+    if (this.editForm.dirty) {
+      $event.returnValue = true;
+    }
+  }
 
   constructor(
     private accountService: AccountService,
-    private memberService: MembersService
+    private memberService: MembersService,
+    private toastr: ToastrService
   ) {
     this.accountService.currentUser$
       .pipe(take(1))
@@ -36,5 +47,12 @@ export class MemberEditComponent implements OnInit {
     this.memberService
       .getMember(this.user.userName)
       .subscribe((member) => (this.member = member));
+  }
+
+  updateMember(): void {
+    this.memberService.updateMember(this.member).subscribe(() => {
+      this.toastr.success('Profile updated successfully!');
+      this.editForm.reset(this.member);
+    });
   }
 }
