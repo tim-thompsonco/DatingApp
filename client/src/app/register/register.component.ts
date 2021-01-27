8feac1187e704f6fd59ac1655cf215d0bcc0d00b
Tim Thompson <scubatim84@gmail.com>
2021-01-27 08:11:51 -0700

    Add validation feedback to register form

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -33,7 +33,7 @@ export class RegisterComponent implements OnInit {
 
   initializeForm(): void {
     this.registerForm = new FormGroup({
-      username: new FormControl('Test', Validators.required),
+      username: new FormControl('', Validators.required),
       password: new FormControl('', [
         Validators.required,
         Validators.minLength(4),
