37e105ea6840d1df1c7f2414dfe2c35dee5e8e4f
Tim Thompson <scubatim84@gmail.com>
2021-02-05 06:24:29 -0700

    Expand register form inputs

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -35,7 +35,12 @@ export class RegisterComponent implements OnInit {
 
   initializeForm(): void {
     this.registerForm = this.formBuilder.group({
+      gender: ['male'],
       username: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
       password: [
         '',
         [Validators.required, Validators.minLength(4), Validators.maxLength(8)],
