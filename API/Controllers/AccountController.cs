f5c3cc83073c9f5e5dfe363acd33940e904f39f4
Tim Thompson <scubatim84@gmail.com>
2021-02-12 13:23:18 -0700

    Add additional fields for user from registration to API

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -2,6 +2,7 @@ using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
@@ -13,8 +14,10 @@ namespace API.Controllers {
 	public class AccountController : BaseApiController {
 		private readonly DataContext _context;
 		private readonly ITokenService _tokenService;
+		private readonly IMapper _mapper;
 
-		public AccountController(DataContext context, ITokenService tokenService) {
+		public AccountController(DataContext context, ITokenService tokenService, IMapper mapper) {
+			_mapper = mapper;
 			_tokenService = tokenService;
 			_context = context;
 		}
@@ -25,21 +28,21 @@ namespace API.Controllers {
 				return BadRequest("Username is taken.");
 			}
 
+			var user = _mapper.Map<AppUser>(registerDto);
+
 			using var hmac = new HMACSHA512();
 
-			var user = new AppUser {
-				UserName = registerDto.UserName.ToLower(),
-				KnownAs = registerDto.UserName,
-				PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-				PasswordSalt = hmac.Key
-			};
+			user.UserName = registerDto.UserName.ToLower();
+			user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
+			user.PasswordSalt = hmac.Key;
 
 			_context.Users.Add(user);
 			await _context.SaveChangesAsync();
 
 			return new UserDto {
 				UserName = user.UserName,
-				Token = _tokenService.CreateToken(user)
+				Token = _tokenService.CreateToken(user),
+				KnownAs = user.KnownAs
 			};
 		}
 
@@ -64,7 +67,8 @@ namespace API.Controllers {
 			return new UserDto {
 				UserName = user.UserName,
 				Token = _tokenService.CreateToken(user),
-				PhotoUrl = user.Photos.FirstOrDefault(photo => photo.IsMain)?.Url
+				PhotoUrl = user.Photos.FirstOrDefault(photo => photo.IsMain)?.Url,
+				KnownAs = user.KnownAs
 			};
 		}
 
