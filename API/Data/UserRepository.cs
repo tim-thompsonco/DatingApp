90cb77dcc18a5d0d88265364b7e4ffadb9f5847b
Tim Thompson <scubatim84@gmail.com>
2021-02-15 07:44:09 -0700

    Add age filter to API

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -5,6 +5,7 @@ using API.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -52,8 +53,14 @@ namespace API.Data {
 
 		public async Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams) {
 			var query = _context.Users.AsQueryable();
+
 			query = query.Where(user => user.UserName != userParams.CurrentUsername);
 			query = query.Where(user => user.Gender == userParams.Gender);
+
+			var minDob = DateTime.Today.AddYears(-userParams.MaxAge - 1);
+			var maxDob = DateTime.Today.AddYears(-userParams.MinAge);
+
+			query = query.Where(user => user.DateOfBirth >= minDob && user.DateOfBirth <= maxDob);
 
 			return await PagedList<MemberDto>.CreateAsync(query.ProjectTo<MemberDto>(_mapper
 			  .ConfigurationProvider).AsNoTracking(), userParams.PageNumber, userParams.PageSize);
