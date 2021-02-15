98b8789afe2e6a0004a8b8bb9a2a3203f62dccde
Tim Thompson <scubatim84@gmail.com>
2021-02-15 13:17:25 -0700

    Fix formatting for CreateToken method, remove BuggyController class

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -17,10 +17,9 @@ namespace API.Services {
 		}
 
 		public string CreateToken(AppUser user) {
-			var claims = new List<Claim>
-			{
+			var claims = new List<Claim> {
 				new Claim(JwtRegisteredClaimNames.NameId, user.Id.ToString()),
-			new Claim(JwtRegisteredClaimNames.UniqueName, user.UserName)
+			  new Claim(JwtRegisteredClaimNames.UniqueName, user.UserName)
 			};
 
 			var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
