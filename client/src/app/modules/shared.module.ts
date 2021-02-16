91e9b041740913e11528a14a2b1a54fc639b5071
Tim Thompson <scubatim84@gmail.com>
2021-02-15 19:47:07 -0700

    Clean up date formatting in client

--- client/src/app/modules/shared.module.ts
+++ client/src/app/modules/shared.module.ts
@@ -9,7 +9,7 @@ import { FileUploadModule } from 'ng2-file-upload';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
-import { Button } from 'protractor';
+import { TimeagoModule } from 'ngx-timeago';
 
 @NgModule({
   declarations: [],
@@ -25,6 +25,7 @@ import { Button } from 'protractor';
     BsDatepickerModule.forRoot(),
     PaginationModule.forRoot(),
     ButtonsModule.forRoot(),
+    TimeagoModule.forRoot(),
   ],
   exports: [
     BsDropdownModule,
@@ -35,6 +36,7 @@ import { Button } from 'protractor';
     BsDatepickerModule,
     PaginationModule,
     ButtonsModule,
+    TimeagoModule,
   ],
 })
 export class SharedModule {}
