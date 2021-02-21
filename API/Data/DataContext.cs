ba9921c4338e6194a28b0fb11ded85298712c364
Tim Thompson <scubatim84@gmail.com>
2021-02-21 15:19:59 -0700

    Wire up API to handle users being able to like each other

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -4,8 +4,27 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Data {
 	public class DataContext : DbContext {
 		public DbSet<AppUser> Users { get; set; }
+		public DbSet<UserLike> Likes { get; set; }
 
 		public DataContext(DbContextOptions options) : base(options) {
+		}
+
+		protected override void OnModelCreating(ModelBuilder builder) {
+			base.OnModelCreating(builder);
+
+			builder.Entity<UserLike>().HasKey(key => new { key.SourceUserId, key.LikedUserId });
+
+			builder.Entity<UserLike>()
+			  .HasOne(source => source.SourceUser)
+			  .WithMany(likes => likes.LikedUsers)
+			  .HasForeignKey(source => source.SourceUserId)
+			  .OnDelete(DeleteBehavior.Cascade);
+
+			builder.Entity<UserLike>()
+					.HasOne(liked => liked.LikedUser)
+					.WithMany(likedBy => likedBy.LikedByUsers)
+					.HasForeignKey(source => source.LikedUserId)
+					.OnDelete(DeleteBehavior.Cascade);
 		}
 	}
 }
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,4 +1,3 @@
-using API.Extensions;
 using System;
 using System.Collections.Generic;
 
@@ -19,10 +18,7 @@ namespace API.Entities {
 		public string City { get; set; }
 		public string Country { get; set; }
 		public ICollection<Photo> Photos { get; set; }
-
-		// public int GetAge()
-		// {
-		// 	return DateOfBirth.CalculateAge();
-		// }
+		public ICollection<UserLike> LikedByUsers { get; set; }
+		public ICollection<UserLike> LikedUsers { get; set; }
 	}
 }
