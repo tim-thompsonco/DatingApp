363ad2837ef78b08e3e05600ef9bf363f3dd960c
Tim Thompson <scubatim84@gmail.com>
2021-02-28 14:23:08 -0700

    Configure IdentityServiceExtention to use .NET Identity

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -1,4 +1,7 @@
+using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
@@ -8,6 +11,15 @@ namespace API.Extensions {
 	public static class IdentityServiceExtensions {
 		public static IServiceCollection AddIdentityServices(
 			this IServiceCollection services, IConfiguration config) {
+			services.AddIdentityCore<AppUser>(options => {
+				options.Password.RequireNonAlphanumeric = false;
+			})
+			  .AddRoles<AppRole>()
+			  .AddRoleManager<RoleManager<AppRole>>()
+			  .AddSignInManager<SignInManager<AppUser>>()
+			  .AddRoleValidator<RoleValidator<AppRole>>()
+			  .AddEntityFrameworkStores<DataContext>();
+
 			services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 				.AddJwtBearer(options => {
 					options.TokenValidationParameters = new TokenValidationParameters {
