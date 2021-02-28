3ca6f9dac58fe5fae41029e40b7559d4d254ba9a
Tim Thompson <scubatim84@gmail.com>
2021-02-28 14:17:21 -0700

    Implement ASP Identity in DataContext

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -1,9 +1,12 @@
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data {
-	public class DataContext : DbContext {
-		public DbSet<AppUser> Users { get; set; }
+	public class DataContext : IdentityDbContext<AppUser, AppRole, int,
+  IdentityUserClaim<int>, AppUserRole, IdentityUserLogin<int>,
+  IdentityRoleClaim<int>, IdentityUserToken<int>> {
 		public DbSet<UserLike> Likes { get; set; }
 		public DbSet<Message> Messages { get; set; }
 
@@ -12,6 +15,18 @@ namespace API.Data {
 
 		protected override void OnModelCreating(ModelBuilder builder) {
 			base.OnModelCreating(builder);
+
+			builder.Entity<AppUser>()
+			  .HasMany(ur => ur.UserRoles)
+			  .WithOne(u => u.User)
+			  .HasForeignKey(ur => ur.UserId)
+			  .IsRequired();
+
+			builder.Entity<AppRole>()
+			  .HasMany(ur => ur.UserRoles)
+			  .WithOne(r => r.Role)
+			  .HasForeignKey(ur => ur.RoleId)
+			  .IsRequired();
 
 			builder.Entity<UserLike>().HasKey(key => new { key.SourceUserId, key.LikedUserId });
 
