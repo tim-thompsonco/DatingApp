3078a303dd4e7209ab526ec1f0d2fd65ce93e951
Tim Thompson <scubatim84@gmail.com>
2021-03-01 08:29:23 -0700

    Implement API route to get list of users who have assigned roles

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -1,12 +1,34 @@
+using API.Entities;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace API.Controllers {
 	public class AdminController : BaseApiController {
+		private readonly UserManager<AppUser> _userManager;
+
+		public AdminController(UserManager<AppUser> userManager) {
+			_userManager = userManager;
+		}
+
 		[Authorize(Policy = "RequireAdminRole")]
 		[HttpGet("users-with-roles")]
-		public ActionResult GetUsersWithRoles() {
-			return Ok("Only admins can see this");
+		public async Task<ActionResult> GetUsersWithRoles() {
+			var users = await _userManager.Users
+			  .Include(r => r.UserRoles)
+			  .ThenInclude(r => r.Role)
+			  .OrderBy(user => user.UserName)
+			  .Select(user => new {
+				  user.Id,
+				  Username = user.UserName,
+				  Roles = user.UserRoles.Select(r => r.Role.Name).ToList()
+			  })
+			  .ToListAsync();
+
+			return Ok(users);
 		}
 
 		[Authorize(Policy = "ModeratePhotoRole")]
