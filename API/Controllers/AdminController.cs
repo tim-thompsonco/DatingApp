485447c68384c399c9d08b948ab31ba86bb3ae9e
Tim Thompson <scubatim84@gmail.com>
2021-03-01 08:37:39 -0700

    Implement API route to edit roles for existing user

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -29,6 +30,33 @@ namespace API.Controllers {
 			  .ToListAsync();
 
 			return Ok(users);
+		}
+
+		[HttpPost("edit-roles/{username}")]
+		public async Task<ActionResult> EditRoles(string username, [FromQuery] string roles) {
+			string[] selectedRoles = roles.Split(",").ToArray();
+
+			AppUser user = await _userManager.FindByNameAsync(username);
+
+			if (user == null) {
+				return NotFound("Could not find user");
+			}
+
+			IList<string> userRoles = await _userManager.GetRolesAsync(user);
+
+			IdentityResult result = await _userManager.AddToRolesAsync(user, selectedRoles.Except(userRoles));
+
+			if (!result.Succeeded) {
+				return BadRequest("Failed to add new roles for user");
+			}
+
+			result = await _userManager.RemoveFromRolesAsync(user, userRoles.Except(selectedRoles));
+
+			if (!result.Succeeded) {
+				return BadRequest("Failed to remove old roles for user");
+			}
+
+			return Ok(await _userManager.GetRolesAsync(user));
 		}
 
 		[Authorize(Policy = "ModeratePhotoRole")]
