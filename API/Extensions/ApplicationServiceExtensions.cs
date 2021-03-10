bc5c3c903716e748f1c93182d11c8941d620f0dd
Tim Thompson <scubatim84@gmail.com>
2021-03-10 06:29:25 -0700

    Change DB source from SQLite to PostgreSQL, add production build folder to git ignore

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -20,7 +20,7 @@ namespace API.Extensions {
             services.AddScoped<LogUserActivity>();
             services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
             services.AddDbContext<DataContext>(options => {
-                options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+                options.UseNpgsql(config.GetConnectionString("DefaultConnection"));
             });
 
             return services;
